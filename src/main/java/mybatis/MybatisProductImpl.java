00e42e8af928e221819a982779bdb73db19f512b
teotiwg <teotiwg@gmail.com>
2021-08-27 21:09:16 +0900

    0827 은서  페이징/ 정렬 완성
    but. 중복적용이 안된다능... 수정 필요 ㅠ 원인불명 ㅠㅠ

--- src/main/java/mybatis/MybatisProductImpl.java
+++ src/main/java/mybatis/MybatisProductImpl.java
@@ -8,32 +8,44 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface MybatisProductImpl {
-
+	// 특정 상품 불러오기
+	public ProductDTO getProductDTO(String p_idx);;
+	
 	// 스토어 리스트 불러오기
-	public ProductDTO getProductDTO();
 	public List<ProductDTO> getStoreList(ProductDTO productDTO);
+	// 페이징처리한 리스트 불러오기
 	//public List<ProductDTO> getPagedList(int start, int end);
 	public List<ProductDTO> getPagedList(@Param("start")int start,
-										@Param("end") int end);
+										@Param("end") int end,
+										@Param("order") String order,
+										@Param("flag") String flag);
 	//public List<ProductDTO> getPagedList(Map<Integer, Integer> range);
-	public List<ProductDTO> getPagedList();
+	//public List<ProductDTO> getPagedList();
+	
+	// 페이징 위한 상품 수 카운트
 	public int getTotalCount();
 	
+	// 가격정렬
 	public List<ProductDTO> descPrice(ProductDTO productDTO);
 	public List<ProductDTO> ascPrice(ProductDTO productDTO);
 	
+	// 상품/디테일 입력
 	public void insertProduct(ProductDTO productDTO);
 	public void insertDetail(ProductDetailDTO detailDTO);
 
+	// 상세페이지 정보 가져오기 
 	public ProductDTO getStoreDetail(String p_idx);
-
 	public ProductDetailDTO getProductDetail(String p_code);
 	
+	// 상품/디테일 업데이트
 	public void updateProduct(ProductDTO productDTO);
 	public void updateDetail(ProductDetailDTO productDetailDTO);
 	
+	// 상품/디테일 삭제
 	public void deleteProduct(String p_idx);
+	public void deleteDetail(String p_code);
 	
+	// 리뷰들~!
 	public List<ReviewDTO> getReviews(String p_code);
 	public void insertReview(ReviewDTO reviewDTO);
 	public void updateReview(ReviewDTO reviewDTO);
