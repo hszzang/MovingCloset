2cde702974388254006902aa21582749db1fc053
teotiwg <teotiwg@gmail.com>
2021-08-28 21:48:54 +0900

    0828 은서 우는 중 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- src/main/java/mybatis/MybatisProductImpl.java
+++ src/main/java/mybatis/MybatisProductImpl.java
@@ -13,17 +13,25 @@ public interface MybatisProductImpl {
 	
 	// 스토어 리스트 불러오기
 	public List<ProductDTO> getStoreList(ProductDTO productDTO);
+	//public List<ProductDTO> getOrderedList(@Param("order") String order,
+	//									@Param("flag") String flag);
 	// 페이징처리한 리스트 불러오기
-	//public List<ProductDTO> getPagedList(int start, int end);
-	public List<ProductDTO> getPagedList(@Param("start")int start,
-										@Param("end") int end,
+	public List<ProductDTO> getPagedList(@Param("flag") String flag,
 										@Param("order") String order,
-										@Param("flag") String flag);
+										@Param("start")int start,
+										@Param("end") int end);
+	/*
+	public List<ProductDTO> getPagedList(List<ProductDTO> list,
+										@Param("start")int start,
+										@Param("end") int end
+										);
+	*/
 	//public List<ProductDTO> getPagedList(Map<Integer, Integer> range);
 	//public List<ProductDTO> getPagedList();
 	
 	// 페이징 위한 상품 수 카운트
-	public int getTotalCount();
+	public int getCount(@Param("flag") String flag);
+	//public int getCount(List<ProductDTO> list);
 	
 	// 가격정렬
 	public List<ProductDTO> descPrice(ProductDTO productDTO);
