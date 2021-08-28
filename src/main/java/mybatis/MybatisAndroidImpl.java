2ac0d2dedb08d9cc7219037db8c79899b03696dc
hszzang <qpwohs2@gmail.com>
2021-08-28 19:50:45 +0900

    안드로이드 내 모여리스트 RestAPI 생성 완

--- src/main/java/mybatis/MybatisAndroidImpl.java
+++ src/main/java/mybatis/MybatisAndroidImpl.java
@@ -12,5 +12,14 @@ public interface MybatisAndroidImpl {
 	//안드로이드 로그인하기
 	public MemberDTO androidLogin(String userid, String userpass);
 	
+	//안드로이드 모여리스트 가져오기
+	
+	//안드로이드 내 모여리스트 보기
+	public ArrayList<MoyoDTO> andGetMyMoyoList(String userid);
+	
+	//안드로이드 내 모여 신청 정보 보기
+	public MoyoUseDTO andGetMyMoyoInfo(String m_idx, String userid);
+	
+	
 
 }
