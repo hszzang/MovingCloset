963f2880704e03ab66d473c0c228e9ae31346dc3
teotiwg <teotiwg@gmail.com>
2021-08-29 20:54:01 +0900

    0829 은서 검색기능 진행중
    해결해야할 문제들..
    - 라디오버튼 동그라미부분 어떻게 지우나?
      + 라디오버튼으로 계속 갈 것인지 쳌박스로 바꿀지 논의 필요
    - 색상 검색 외부조인으로 하는데 오라클에선 되지만 이클립스엔선 안됌...
      badsql예외 뜸..
    - 그리고 검색하면 페이지에 뜨는 검색 결과가 지 멋대로 한정됌 ㅡㅡ
    - 정렬도 제대로 안 됌 ㅠㅠ 시발!

--- src/main/java/movingcloset/controller/StoreController.java
+++ src/main/java/movingcloset/controller/StoreController.java
@@ -11,7 +11,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -19,7 +18,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
